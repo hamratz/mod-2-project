88b9e157699515453c02a8e6d9536bed029dcd40
James Sapp <jameseisapp@gmail.com>
2021-01-26 09:22:20 -0600

    cart udpated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,20 @@
 class ApplicationController < ActionController::Base
-    # before_action :get_item_from_cart
-    helper_method :cart
+    before_action :get_item_from_cart
+    # this happens first
 
   def cart 
     session[:cart] ||= []
   end
 
   def add_item_to_cart(item_id)
-    # @item = Item.find(params[:id])
-    cart << { id: item_id }
+    cart << item_id
     flash[:notice] = "item added to cart!"
   end
 
-#   def get_item_from_cart
-#     @cart_items = item.find(cart)
-#   end
+  # helper method for id's in cart 
+  def get_item_from_cart
+    @cart_items = Item.find(cart)
+  end
 
 
 end
