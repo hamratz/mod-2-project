07eff034b3526b808f494ba0497ca9f467bee4d3
James Sapp <jameseisapp@gmail.com>
2021-01-27 19:27:04 -0600

    completed cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,21 +1,31 @@
 class CartsController < ApplicationController
-
-    # add item to cart - i think we need to move this
+    # skip_before_action :get_item_from_cart, only: [:show, :edit]
+    
     def update
-      add_item_to_cart(params[:item_id])
+      byebug
+      cart << params[:item_id]
+      flash[:notice] = "item added to cart!"
       redirect_to request.referrer
     end
 
     def show
-     get_item_from_cart
+      @hide_cart = true
     end
 
     def edit
-      get_item_from_cart
+      @hide_cart = true
     end
 
-    # remove item from cart
+    # need to do
+    def remove_item
+
+      session[:cart].delete(params[:item_id])
+      redirect_to request.referrer
+    end
+
+    # checkout - add redirect_to and flash checkout
     def destroy
+      session.delete(:cart)
       render :checkout
     end
 
