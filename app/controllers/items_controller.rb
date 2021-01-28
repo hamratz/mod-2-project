9f9f66c83536e2c481716bd9d5d3c9d34ee6da44
esmasher <esmasherr@gmail.com>
2021-01-28 09:16:42 -0600

    Item links

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
     skip_before_action :authorize, only: [:show, :index]
-
+    def index
+        @items = Item.all.shuffle.first
+    end
 
 
     def show
